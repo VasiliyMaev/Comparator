0103e044fb82500a4fa16ed6c1b51e984e0a9b77
Vasiliy Maev <sweekom@gmail.com>
2022-10-27 16:25:17 +0300

    delete person < 18 age

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
         personList.add(new Person("Котофей", "Пупочкин", 23));
-        personList.add(new Person("Иван", "Табуреткин-Заводской", 42));
+        personList.add(new Person("Иван", "Табуреткин-Заводской", 12));
         personList.add(new Person("Катерина", "Барабанова Трубская Баринова Лапочкина", 44));
         personList.add(new Person("Софья", "ДранниковаБлинчиковаГолубцоваСметанина", 22));
         personList.add(new Person("Ян", "У И Ля Сяо Ху", 13));
 
         SurnameLength surnameLength = new SurnameLength(3);
+
+        personList.removeIf(p -> (p.getAge() < 18));
 
         personList.sort(surnameLength);
         System.out.println(personList);
